9ea08e02625158cf3b623cdc77a3d60922096a18
DiogoTofuMartins <rapyourself@gmail.com>
2021-02-11 12:50:27 -0100

    esp8266 config

---
+++ POV-esp8266-configuration/POV-esp8266-configuration.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+
+#define NAME "MEO-739510"
+#define PASS "fb5cd86358"
+
+const char* host = "192.168.1.71";
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+
+  /* Set Client up as station */
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(NAME, PASS);
+
+  /* Connect to the network */
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (client.connect(host, 9090)) {
+    Serial.print("Connected to: ");
+    Serial.println(host);
+  }
+}
+void loop() {
+
+
+
+  if (client.available()) {
+   
+    String data = client.readStringUntil('\n');
+    Serial.print("POV message: ");
+    Serial.println(data);
+  }
+
+
+}
